9338954ce02d3ad27df8e23169b228e0b2809034
GishB <SamofalovWORK@yandex.ru>
2024-02-08 01:44:42 +0300

    fix some pep8 error \ renaming yml

--- YandexAlgorithms/2022/Practic_Algorithm_September/Yandex_school_test_A.py
+++ YandexAlgorithms/2022/Practic_Algorithm_September/Yandex_school_test_A.py
@@ -1,86 +1,76 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
-a = input()
-b = input()
-
-
-# In[5]:
-
-
-if __name__ == '__main__':
-    import numpy as np
-    initial_id = np.random.randint(0,3)
-    a_list = ['CLOUD', 'ALICE', 'ABCBCYA']
-    b_list = ['CUPID', 'ELIBO', 'ZBBACAA']
-    a = a_list[initial_id]
-    print(a)
-    b = b_list[initial_id]
-    print(b)
-
-
-# In[6]:
-
-
-result = []
-list_A = list(a)
-list_B = list(b)
-
-for i in range(len(list_B)):
-    list_check_A = []
-    memory_list = []
-    memory_count = 0
-    memory_count_2 = 0
-
-    temp_B = list_B[i]
-    temp_A = list_A[i]
-
-    if temp_B == temp_A:
-        result.append('P')
-
-    elif temp_B in list_A:
-        count_s_cases = 0
-        for g in range(len(list_A)):
-            if temp_B == list_A[g] and list(b)[g] != list_A[g]:
-                count_s_cases += 1
-        count_s_cases_in_B = 0
-        list_s_cases_in_B = []
-        for g in range(len(list_A)):
-            if temp_B == list(b)[g] and list(b)[g] != list_A[g]:
-                count_s_cases_in_B += 1
-                list_s_cases_in_B.append(1)
-            else:
-                list_s_cases_in_B.append(0)
-        if count_s_cases_in_B == count_s_cases:
-            result.append('S')
-        else: #*Кол-во связей было нечетным
-            #Запустим цикл по всей строке и расставим S случаи по очереди слева направо в list_s_cases_in_B
-            for g in range(len(list_A)):
-                if list_s_cases_in_B[g] != 0:
-                    list_s_cases_in_B[g] = 0 #Найдена пара к b[i] из верхнего списка А[a[i],...a[i-1]]
-                    count_s_cases = count_s_cases - 1 #Кол-во S-связей для текущей буквы в A уменьшилось т.к. есть пара
-                    count_s_cases_in_B = count_s_cases_in_B - 1 #Кол-во S-связей для текущей буквы уменьшилось в B
-                if count_s_cases == 0: #Если кол-во случаев S-связей для списка A с B = 0, то цикл СТОП
-                    break
-                else: #Продолжаем искать пару к b[i] из верхнего списка А[a[i],...a[i-1]] пока count_s_cases != 0
-                    pass
-            #Проверим текущей индекс i  - совпадает с значением 1 в листке list_connected_s_cases? 
-            if list_s_cases_in_B[i] == 1: #Если значение есть, то получается эта буква правее всех случаев S
-                result.append('I')
-            else: #Если значение != 1, то получается для этой буквы есть связь в верхнем регистре
-                #*ВАЖНО* связь условная, очень, запутанно тут
-                result.append('S')
-    else:
-        result.append('I')
-
-print(''.join(result)) #вывод ответа
-
-
-# In[ ]:
-
-
-
-
+#
+# a = input()
+# b = input()
+#
+#
+# if __name__ == '__main__':
+#     import numpy as np
+#     initial_id = np.random.randint(0,3)
+#     a_list = ['CLOUD', 'ALICE', 'ABCBCYA']
+#     b_list = ['CUPID', 'ELIBO', 'ZBBACAA']
+#     a = a_list[initial_id]
+#     print(a)
+#     b = b_list[initial_id]
+#     print(b)
+#
+#
+# # In[6]:
+#
+#
+# result = []
+# list_A = list(a)
+# list_B = list(b)
+#
+# for i in range(len(list_B)):
+#     list_check_A = []
+#     memory_list = []
+#     memory_count = 0
+#     memory_count_2 = 0
+#
+#     temp_B = list_B[i]
+#     temp_A = list_A[i]
+#
+#     if temp_B == temp_A:
+#         result.append('P')
+#
+#     elif temp_B in list_A:
+#         count_s_cases = 0
+#         for g in range(len(list_A)):
+#             if temp_B == list_A[g] and list(b)[g] != list_A[g]:
+#                 count_s_cases += 1
+#         count_s_cases_in_B = 0
+#         list_s_cases_in_B = []
+#         for g in range(len(list_A)):
+#             if temp_B == list(b)[g] and list(b)[g] != list_A[g]:
+#                 count_s_cases_in_B += 1
+#                 list_s_cases_in_B.append(1)
+#             else:
+#                 list_s_cases_in_B.append(0)
+#         if count_s_cases_in_B == count_s_cases:
+#             result.append('S')
+#         else: #*Кол-во связей было нечетным
+#             #Запустим цикл по всей строке и расставим S случаи по очереди слева направо в list_s_cases_in_B
+#             for g in range(len(list_A)):
+#                 if list_s_cases_in_B[g] != 0:
+#                     list_s_cases_in_B[g] = 0 #Найдена пара к b[i] из верхнего списка А[a[i],...a[i-1]]
+#                     count_s_cases = count_s_cases - 1 #Кол-во S-связей для текущей буквы в A уменьшилось т.к. есть пара
+#                     count_s_cases_in_B = count_s_cases_in_B - 1 #Кол-во S-связей для текущей буквы уменьшилось в B
+#                 if count_s_cases == 0: #Если кол-во случаев S-связей для списка A с B = 0, то цикл СТОП
+#                     break
+#                 else: #Продолжаем искать пару к b[i] из верхнего списка А[a[i],...a[i-1]] пока count_s_cases != 0
+#                     pass
+#             #Проверим текущей индекс i  - совпадает с значением 1 в листке list_connected_s_cases?
+#             if list_s_cases_in_B[i] == 1: #Если значение есть, то получается эта буква правее всех случаев S
+#                 result.append('I')
+#             else: #Если значение != 1, то получается для этой буквы есть связь в верхнем регистре
+#                 #*ВАЖНО* связь условная, очень, запутанно тут
+#                 result.append('S')
+#     else:
+#         result.append('I')
+#
+# print(''.join(result)) #вывод ответа
+#
+#
+#
+#
+#
--- YandexAlgorithms/2022/Practic_Algorithm_September/Yandex_school_test_C.py
+++ YandexAlgorithms/2022/Practic_Algorithm_September/Yandex_school_test_C.py
@@ -1,70 +1,70 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-input_list = input()
-PRICE_LESS_THAN = input()
-DATE_AFTER = input()
-NAME_CONTAINS = input()
-PRICE_GREATER_THAN = input()
-DATE_BEFORE = input()
-
-
-# In[2]:
-
-
-import json
-import time
-
-
-# In[3]:
-
-
-if __name__ == '__main__':
-    input_list = [{"id": 1, "name": "Asus notebook","price": 1564,"date": "23.09.2021"},{"price": 2500, "id": 3, "date": "05.06.2020", "name": "Keyboardpods" }, {"date": "23.09.2021", "name": "Airpods","id": 5, "price": 2300}, {"name": "EaRPoDs", "id": 2, "date": "01.01.2022", "price": 2200}, { "id": 4, "date": "23.09.2021", "name": "Dell notebook",  "price": 2300}]
-    PRICE_LESS_THAN = 2400
-    DATE_AFTER = '23.09.2021'
-    NAME_CONTAINS = 'pods'
-    PRICE_GREATER_THAN = 2200
-    DATE_BEFORE = '02.01.2022'
-
-
-# In[4]:
-
-
-new_test = list(input_list)
-
-
-# In[5]:
-
-
-#Переводим время в формат time date/month/year
-DATE_AFTER = DATE_AFTER.replace('.','/')
-DATE_AFTER = time.strptime(DATE_AFTER, "%d/%m/%Y")
-DATE_BEFORE = DATE_BEFORE.replace('.','/')
-DATE_BEFORE = time.strptime(DATE_BEFORE, "%d/%m/%Y")
-
-
-# In[6]:
-
-
-size_json = len(new_test)
-list_id = []
-for i in range(size_json):
-    temp_json = new_test[i]
-    if NAME_CONTAINS in temp_json['name'].lower():
-        if temp_json['price'] <= PRICE_LESS_THAN and temp_json['price'] >= PRICE_GREATER_THAN:
-            json_data = temp_json['date'].replace('.','/')
-            json_data = time.strptime(json_data, "%d/%m/%Y")
-            if DATE_AFTER >= json_data and json_data <= DATE_BEFORE:
-                list_id.append([i,temp_json['id']])
-                
-new_sort_list = sorted(list_id, key=lambda x: x[1])
-output_list_json = []
-for i in range(len(new_sort_list)):
-    id_sort = new_sort_list[i][0]
-    output_list_json.append(new_test[id_sort])
-print(output_list_json)
-
+# #!/usr/bin/env python
+# # coding: utf-8
+#
+# # In[1]:
+#
+#
+# input_list = input()
+# PRICE_LESS_THAN = input()
+# DATE_AFTER = input()
+# NAME_CONTAINS = input()
+# PRICE_GREATER_THAN = input()
+# DATE_BEFORE = input()
+#
+#
+# # In[2]:
+#
+#
+# import json
+# import time
+#
+#
+# # In[3]:
+#
+#
+# if __name__ == '__main__':
+#     input_list = [{"id": 1, "name": "Asus notebook","price": 1564,"date": "23.09.2021"},{"price": 2500, "id": 3, "date": "05.06.2020", "name": "Keyboardpods" }, {"date": "23.09.2021", "name": "Airpods","id": 5, "price": 2300}, {"name": "EaRPoDs", "id": 2, "date": "01.01.2022", "price": 2200}, { "id": 4, "date": "23.09.2021", "name": "Dell notebook",  "price": 2300}]
+#     PRICE_LESS_THAN = 2400
+#     DATE_AFTER = '23.09.2021'
+#     NAME_CONTAINS = 'pods'
+#     PRICE_GREATER_THAN = 2200
+#     DATE_BEFORE = '02.01.2022'
+#
+#
+# # In[4]:
+#
+#
+# new_test = list(input_list)
+#
+#
+# # In[5]:
+#
+#
+# #Переводим время в формат time date/month/year
+# DATE_AFTER = DATE_AFTER.replace('.','/')
+# DATE_AFTER = time.strptime(DATE_AFTER, "%d/%m/%Y")
+# DATE_BEFORE = DATE_BEFORE.replace('.','/')
+# DATE_BEFORE = time.strptime(DATE_BEFORE, "%d/%m/%Y")
+#
+#
+# # In[6]:
+#
+#
+# size_json = len(new_test)
+# list_id = []
+# for i in range(size_json):
+#     temp_json = new_test[i]
+#     if NAME_CONTAINS in temp_json['name'].lower():
+#         if temp_json['price'] <= PRICE_LESS_THAN and temp_json['price'] >= PRICE_GREATER_THAN:
+#             json_data = temp_json['date'].replace('.','/')
+#             json_data = time.strptime(json_data, "%d/%m/%Y")
+#             if DATE_AFTER >= json_data and json_data <= DATE_BEFORE:
+#                 list_id.append([i,temp_json['id']])
+#
+# new_sort_list = sorted(list_id, key=lambda x: x[1])
+# output_list_json = []
+# for i in range(len(new_sort_list)):
+#     id_sort = new_sort_list[i][0]
+#     output_list_json.append(new_test[id_sort])
+# print(output_list_json)
+#
