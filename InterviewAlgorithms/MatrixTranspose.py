23226f76908db024986dbd30a44c3d231c237b56
GishB <SamofalovWORK@yandex.ru>
2024-03-22 01:22:22 +0300

    add matrix transpose

---
+++ InterviewAlgorithms/MatrixTranspose.py
@@ -0,0 +1,36 @@
+"""
+Your task is to transpose given matrix without numpy implementation.
+
+in: [[1,2,3],
+    [4,5,6],
+    [7,8,9]]
+
+out: [[1,4,7],
+      [2,5,8],
+      [3,6,9]]
+
+"""
+from itertools import chain
+
+
+def transpose(matrix: list[list[float | int]]) -> list[list[float | int]]:
+    """ This function apply transpose over given matrix.
+
+    Arg:
+        matrix: list of list where all values are float or int types.
+
+    Return:
+        transposed matrix.
+    """
+    matrix_length = len(matrix)
+    L = list(chain(*matrix))
+    return [L[i::matrix_length] for i in range(matrix_length)]
+
+
+if __name__ == "__main__":
+    import numpy as np
+    test_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    # test_matrix = [[1,2],[3,4]]
+    out = transpose(test_matrix)
+    numpy_test = np.array(test_matrix).T
+    test = 0
